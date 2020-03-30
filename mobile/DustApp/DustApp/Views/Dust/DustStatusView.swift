32fcb2c768d82476bdfba8f5accacf6cfba081a1
corykim0829 <corykim0829@gmail.com>
2020-03-30 20:47:32 +0900

    [#3] Feat: Generate color assets

--- mobile/DustApp/DustApp/Views/Dust/DustStatusView.swift
+++ mobile/DustApp/DustApp/Views/Dust/DustStatusView.swift
@@ -15,18 +15,18 @@ class DustStatusView: UIView {
     var timeLabel: UILabel!
     var instrumentLabel: UILabel!
     
-    private var gradientColor: CGColor = UIColor.white.cgColor {
+    private var gradientColor: CGColor = UIColor(named: "systemWhite")!.cgColor {
         didSet {
             UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
                 let color = self.gradientColor
-                self.gradientLayer.colors = [color,color,color, UIColor.white.cgColor]
+                self.gradientLayer.colors = [color, color, color, UIColor(named: "systemWhite")!.cgColor]
             })
         }
     }
     
     lazy var gradientLayer: CAGradientLayer = {
         let layer = CAGradientLayer()
-        layer.colors = [gradientColor, gradientColor, gradientColor, UIColor.white.cgColor]
+        layer.colors = [gradientColor, gradientColor, gradientColor, UIColor(named: "systemWhite")!.cgColor]
         layer.locations = [0, 0.3 ,0.6, 1]
         layer.frame = bounds
         return layer
@@ -42,7 +42,7 @@ class DustStatusView: UIView {
     }
     
     private func updateGradientLayerColor(with color: CGColor) {
-        gradientLayer.colors = [color,color,color, UIColor.white.cgColor]
+        gradientLayer.colors = [color,color,color, UIColor(named: "systemWhite")!.cgColor]
     }
     
     private func gradeWord() -> String {
@@ -57,11 +57,11 @@ class DustStatusView: UIView {
     
     private func color() -> CGColor {
         switch dustInfo.grade {
-        case .worst: return #colorLiteral(red: 0.9960784314, green: 0.2078431373, blue: 0.137254902, alpha: 1).cgColor
-        case .bad: return #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1).cgColor
-        case .normal: return #colorLiteral(red: 0, green: 0.8549019608, blue: 0.5254901961, alpha: 1).cgColor
-        case .good: return #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1).cgColor
-        case .none: return UIColor.red.cgColor
+            case .good: return UIColor(named: "good")!.cgColor
+            case .normal: return UIColor(named: "normal")!.cgColor
+            case .bad: return UIColor(named: "bad")!.cgColor
+            case .worst: return UIColor(named: "worst")!.cgColor
+            case .none: return UIColor.white.cgColor
         }
     }
     
