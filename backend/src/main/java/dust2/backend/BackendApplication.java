43e9149ea802b09aca6bb369cd3030fd61175936
beemiel <qnswwrn@gmail.com>
2020-04-01 10:55:21 +0900

    [#16] Refactor: 설정과 관련된 클래스 작성

--- backend/src/main/java/dust2/backend/BackendApplication.java
+++ backend/src/main/java/dust2/backend/BackendApplication.java
@@ -2,54 +2,12 @@ package dust2.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:8080");
-			}
-		};
-	}
-
-	@Bean
-	public Docket newsApi() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.groupName("greetings")
-				.apiInfo(apiInfo())
-				.select()
-				.paths(PathSelectors.ant("/**"))
-				.build();
-	}
-
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder()
-				.title("Dust API")
-				.description("Dust API")
-				.termsOfServiceUrl("http://www-03.ibm.com/software/sla/sladb.nsf/sla/bm?Open")
-				.license("Apache License Version 2.0")
-				.licenseUrl("https://github.com/IBM-Bluemix/news-aggregator/blob/master/LICENSE")
-				.version("2.0")
-				.build();
 	}
 
 }
