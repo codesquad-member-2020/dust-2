70daff84922173499c5b5607066947c367be0021
beemiel <qnswwrn@gmail.com>
2020-04-02 23:54:05 +0900

    Chore: 패키지 추가 후 클래스 이동
    클래스 구조를 좀 더 명확히 하기 위해서 api, config 패키지를 추가한 후 클래스 이동

--- backend/src/main/java/dust2/backend/config/WebConfig.java
+++ backend/src/main/java/dust2/backend/config/WebConfig.java
@@ -1,4 +1,4 @@
-package dust2.backend;
+package dust2.backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
