2b17b628c20f3136dd4b32418f7ad670b4090157
sangwon21 <junothyme@gmail.com>
2020-04-03 02:32:30 +0900

    [#29] 서버에서 받아온 데이터 처리에 필요한 enum 추가
    why: 서버에서 받아온 데이터 처리에 필요한 여러 enum들을 추가했다. 이
    enum들은 서로 짝이 맞게 한 enum의 value값이, 다른 enum의 key값으로
    이용될 수 있도록, 설계되었다.

---
+++ client-web/constants/colors.js
@@ -0,0 +1,13 @@
+export const COLOR_FOR_LABEL = {
+  good: "rgba(61, 172, 247, 0.5)",
+  normal: "rgba(0, 218, 134, 0.5)",
+  bad: "rgba(243, 175, 34, 0.5)",
+  worst: "rgba(250, 89, 78, 0.5)"
+};
+
+export const COLOR_FOR_BACKGROUND = {
+  good: "linear-gradient(180deg, rgba(61, 172, 247, 1), rgba(61, 172, 247, 0))",
+  normal: "linear-gradient(180deg, rgba(0, 218, 134, 1), rgba(0, 218, 134, 0))",
+  bad: "linear-gradient(180deg, rgba(243, 175, 34, 1), rgba(243, 175, 34, 0))",
+  worst: "linear-gradient(180deg, rgba(250, 89, 78, 1), rgba(250, 89, 78, 0))"
+};
