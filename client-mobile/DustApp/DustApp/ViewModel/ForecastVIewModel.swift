be761406e29f2395044abac768412308e1ee03bf
corykim0829 <corykim0829@gmail.com>
2020-04-02 19:47:02 +0900

    [#27] Feat: dummy url 배열 기반으로 이미지 요청

--- client-mobile/DustApp/DustApp/ViewModel/ForecastVIewModel.swift
+++ client-mobile/DustApp/DustApp/ViewModel/ForecastVIewModel.swift
@@ -18,7 +18,22 @@ class ForecastViewModel {
     var downloadImagesCompletion: ((Bool, [UIImage]?) -> Void)?
     
     func requestImages() {
-        images = [#imageLiteral(resourceName: "icon-bad"), #imageLiteral(resourceName: "icon-worst"), #imageLiteral(resourceName: "icon-normal"), #imageLiteral(resourceName: "icon-good")]
-        hasDownloaded = true
+        let imageURLStrings = ForecastNetworkManager.shared.URLStrings
+        var placeholderImages = Array<UIImage?>(repeating: nil, count: imageURLStrings.count)
+        var hasNilImage = true
+        
+        for (index, imageURLString) in imageURLStrings.enumerated() {
+            ForecastNetworkManager.shared.requestData(with: imageURLString) { (data, error) in
+                if let error = error { print(error); return }
+                guard let data = data else { return }
+                guard let image = UIImage(data: data) else { return }
+                placeholderImages[index] = image
+                placeholderImages.forEach { hasNilImage = ($0 == nil) }
+                if !hasNilImage {
+                    self.images = placeholderImages.map{ $0! }
+                    self.hasDownloaded = true
+                }
+            }
+        }
     }
 }
