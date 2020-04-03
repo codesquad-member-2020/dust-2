8175aa816c15a3d8facfbbfbf81104037214f50b
corykim0829 <corykim0829@gmail.com>
2020-04-03 15:49:25 +0900

    [#14] Refactor: Request URL 변수 이름 변경 및 배포된 path로 수정

--- client-mobile/DustApp/DustApp/Models/Dust/DustNetworkManager.swift
+++ client-mobile/DustApp/DustApp/Models/Dust/DustNetworkManager.swift
@@ -16,14 +16,15 @@ class DustNetworkManager: NetworkManagable {
     
     static let shared = DustNetworkManager()
     
-    let dustInfoURL = "http://ec2-15-165-109-219.ap-northeast-2.compute.amazonaws.com:8080"
-    let stationPath: String = "location"
+    let hostURL: String = "http://ec2-15-165-109-219.ap-northeast-2.compute.amazonaws.com"
+    let port: String = "8080"
+    let stationPath: String = "/api/air-quality"
     let latitudeQueryParameter: String = "latitude"
     let longtitudeQueryParameter: String = "longtitude"
     
     func requestData(with item: Any?, completion: @escaping (Data?, Error?) -> Void) {
         guard let location = item as? Location else { return }
-        let stationURL = URL(string: "\(dustInfoURL)/\(stationPath)?\(latitudeQueryParameter)=\(location.latitude)&\(longtitudeQueryParameter)=\(location.longitude)")!
+        let stationURL = URL(string: "\(hostURL):\(port)\(stationPath)?\(latitudeQueryParameter)=\(location.latitude)&\(longtitudeQueryParameter)=\(location.longitude)")!
         let request = URLRequest(url: stationURL)
         URLSession.shared.dataTask(with: request) { (data, _, error) in
             if let error = error { completion(nil, error) }
