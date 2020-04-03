8508d382437235ddfdf2a44bc9159f1f661d3b3a
corykim0829 <corykim0829@gmail.com>
2020-04-03 16:50:20 +0900

    [#51] Feat: 이미지 요청 수정

--- client-mobile/DustApp/DustApp/Models/Forecast/ForecastNetworkManager.swift
+++ client-mobile/DustApp/DustApp/Models/Forecast/ForecastNetworkManager.swift
@@ -12,16 +12,9 @@ class ForecastNetworkManager: NetworkManagable {
     
     static let shared = ForecastNetworkManager()
     
-    let URLStrings = [
-        "https://search.pstatic.net/common?quality=75&amp;direct=true&amp;src=https%3A%2F%2Fcsearch-phinf.pstatic.net%2F20200329_78%2F1585469292761Tsmsz_PNG%2Fimage.png",
-        "https://search.pstatic.net/common?quality=75&amp;direct=true&amp;src=https%3A%2F%2Fcsearch-phinf.pstatic.net%2F20200329_95%2F1585469293046Csc0c_PNG%2Fimage.png",
-        "https://search.pstatic.net/common?quality=75&amp;direct=true&amp;src=https%3A%2F%2Fcsearch-phinf.pstatic.net%2F20200329_197%2F1585469293339WIW8s_PNG%2Fimage.png",
-        "https://search.pstatic.net/common?quality=75&amp;direct=true&amp;src=https%3A%2F%2Fcsearch-phinf.pstatic.net%2F20200329_96%2F1585469293583jUiAm_PNG%2Fimage.png",
-        "https://search.pstatic.net/common?quality=75&amp;direct=true&amp;src=https%3A%2F%2Fcsearch-phinf.pstatic.net%2F20200329_24%2F158546929390936vbA_PNG%2Fimage.png",
-        "https://search.pstatic.net/common?quality=75&amp;direct=true&amp;src=https%3A%2F%2Fcsearch-phinf.pstatic.net%2F20200329_161%2F1585469294189OALn7_PNG%2Fimage.png",
-        "https://search.pstatic.net/common?quality=75&amp;direct=true&amp;src=https%3A%2F%2Fcsearch-phinf.pstatic.net%2F20200329_171%2F1585469294438p2zw7_PNG%2Fimage.png",
-        "https://search.pstatic.net/common?quality=75&amp;direct=true&amp;src=https%3A%2F%2Fcsearch-phinf.pstatic.net%2F20200329_158%2F1585469294699286jz_PNG%2Fimage.png"
-    ]
+    let hostURL: String = "http://ec2-15-165-109-219.ap-northeast-2.compute.amazonaws.com"
+    let port: String = "8080"
+    let stationPath: String = "/api/forecast"
     
     func requestData(with item: Any?, completion: @escaping (Data?, Error?) -> Void) {
         guard let imageURLString = item as? String else { return }
@@ -29,6 +22,17 @@ class ForecastNetworkManager: NetworkManagable {
         URLSession.shared.dataTask(with: imageURL) { (data, _, error) in
             if let error = error { completion(nil, error) }
             if let data = data { completion(data, nil) }
+        }.resume()
+    }
+    
+    func requestForecastData(completion: @escaping ((Forecast?, Error?) -> Void)) {
+        let forecastImagesURL = URL(string: "\(hostURL):\(port)\(stationPath)")!
+        URLSession.shared.dataTask(with: forecastImagesURL) { (data, _, error) in
+            if let error = error { completion(nil, error) }
+            guard let data = data else { return }
+            let decoder = JSONDecoder()
+            guard let forecast = try? decoder.decode(Forecast.self, from: data) else { return }
+            completion(forecast, nil)
         }.resume()
     }
 }
--- client-mobile/DustApp/DustApp/ViewModel/ForecastVIewModel.swift
+++ client-mobile/DustApp/DustApp/ViewModel/ForecastVIewModel.swift
@@ -33,20 +33,23 @@ class ForecastViewModel {
     }
     
     func requestImages() {
-        let imageURLStrings = ForecastNetworkManager.shared.URLStrings
-        var placeholderImages = Array<UIImage?>(repeating: nil, count: imageURLStrings.count)
-        var hasNilImage = true
-        
-        for (index, imageURLString) in imageURLStrings.enumerated() {
-            ForecastNetworkManager.shared.requestData(with: imageURLString) { (data, error) in
-                if let error = error { print(error); return }
-                guard let data = data else { return }
-                guard let image = UIImage(data: data) else { return }
-                placeholderImages[index] = image
-                placeholderImages.forEach { hasNilImage = ($0 == nil) }
-                if !hasNilImage {
-                    self.images = placeholderImages.map{ $0! }
-                    self.hasDownloaded = true
+        ForecastNetworkManager.shared.requestForecastData { (forecast, error) in
+            guard let forecast = forecast else { return }
+            let imageURLStrings = forecast.images
+            var placeholderImages = Array<UIImage?>(repeating: nil, count: imageURLStrings.count)
+            var hasNilImage = true
+            
+            for (index, imageURLString) in imageURLStrings.enumerated() {
+                ForecastNetworkManager.shared.requestData(with: imageURLString) { (data, error) in
+                    if let error = error { print(error); return }
+                    guard let data = data else { return }
+                    guard let image = UIImage(data: data) else { return }
+                    placeholderImages[index] = image
+                    placeholderImages.forEach { hasNilImage = ($0 == nil) }
+                    if !hasNilImage {
+                        self.images = placeholderImages.map{ $0! }
+                        self.hasDownloaded = true
+                    }
                 }
             }
         }
