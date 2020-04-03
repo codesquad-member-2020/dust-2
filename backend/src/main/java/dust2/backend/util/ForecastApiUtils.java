30c7d05d318725da025e4ef6914a2649caa06d0f
beemiel <qnswwrn@gmail.com>
2020-04-03 13:23:13 +0900

    [#25] Feat: 이미지 정렬 추가
    가져온 이미지들을 시간순으로 정렬한다

--- backend/src/main/java/dust2/backend/util/ForecastApiUtils.java
+++ backend/src/main/java/dust2/backend/util/ForecastApiUtils.java
@@ -17,19 +17,21 @@ public class ForecastApiUtils {
     private static final Logger log = LoggerFactory.getLogger(ForecastApiUtils.class);
     private static String today = DateUtils.getToday();
 
-    private static String connectToOpenApi() throws URISyntaxException {
+    public static Forecast getTodayForecastFromOpenApi() throws URISyntaxException, JsonProcessingException {
         String apiURL =
                 "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMinuDustFrcstDspth?searchDate=";
         String serviceKey = "&ServiceKey=ck2PICliE4pVzLMDrn6ujDZn76komYPNmfjWX2yzK3%2FJWAq5ktMEEhH3kqWKwL%2FellxN8aZYFGo3KAXnD6XdLw%3D%3D";
         String returnJson = "&_returnType=json";
 
         URI publicApiRequestUrl = new URI(apiURL + today + serviceKey + returnJson);
+        log.info("publicApiRequestUrl : {}" , publicApiRequestUrl);
+
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject(publicApiRequestUrl, String.class);
+        String response = restTemplate.getForObject(publicApiRequestUrl, String.class);
+        return jsonStringToObject(response);
     }
 
-    public static Forecast getTodayForecastFromOpenApi() throws JsonProcessingException, URISyntaxException {
-        String response = connectToOpenApi();
+    private static Forecast jsonStringToObject(String response) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
 
         JsonNode forecastArray = objectMapper.readTree(response).get("list");
@@ -41,9 +43,11 @@ public class ForecastApiUtils {
 
     private static List<String> getTodayImagesFromJson(JsonNode forecastArray) {
         List<String> images = new ArrayList<>();
+        List<JsonNode> morningForecasts = new ArrayList<>();
+        List<JsonNode> afternoonForecasts = new ArrayList<>();
         JsonNode imageObject;
 
-        //오늘 날짜의  5,11시 발표 미세먼지 이미지만 가져옴
+        //오늘 날짜의 미세먼지 이미지만 가져옴
         for(int i=forecastArray.size()-1; i>=0; i--) {
             imageObject = forecastArray.get(i);
 
@@ -51,15 +55,31 @@ public class ForecastApiUtils {
                 continue;
             }
 
-            if(imageObject.get("dataTime").asText().contains("23시") ||
-                    imageObject.get("dataTime").asText().contains("17시")) {
+            if(!today.equals(imageObject.get("informData").asText())){
                 continue;
             }
 
-            if(today.equals(imageObject.get("informData").asText())){
-                images.add(imageObject.get("imageUrl1").asText());
-                images.add(imageObject.get("imageUrl2").asText());
-                images.add(imageObject.get("imageUrl3").asText());
+            if(imageObject.get("dataTime").asText().contains("23시") || imageObject.get("dataTime").asText().contains("17시")) {
+                afternoonForecasts.add(imageObject);
+                continue;
+            }
+
+            morningForecasts.add(imageObject);
+
+        }
+        images = sortImagesInOrder(morningForecasts);
+        images.addAll(sortImagesInOrder(afternoonForecasts));
+
+        return images;
+    }
+
+    private static List<String> sortImagesInOrder(List<JsonNode> forecasts) {
+        int imageCount = 3;
+        List<String> images = new ArrayList<>();
+
+        for (int i=0; i<imageCount; i++) {
+            for(int j=0; j<forecasts.size(); j++) {
+                images.add(forecasts.get(j).get("imageUrl"+(i+1)).asText());
             }
         }
 
