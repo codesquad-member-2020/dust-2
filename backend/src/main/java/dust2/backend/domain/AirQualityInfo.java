2f3a0bc734bbbdbbb66040a7fe778974cdf18bd1
kyungrae <rlarudfo93@gmail.com>
2020-04-03 15:47:25 +0900

    feat: json 응답 포멧 수정을 위해 AirQualityInfo 타입 수

--- backend/src/main/java/dust2/backend/domain/AirQualityInfo.java
+++ backend/src/main/java/dust2/backend/domain/AirQualityInfo.java
@@ -5,8 +5,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AirQualityInfo {
     private String dataTime;
-    private String pm10Value;
-    private String pm10Grade;
+    private int pm10Value;
+    private int pm10Grade;
     private String stationName;
 
     public String getDataTime() {
@@ -17,19 +17,19 @@ public class AirQualityInfo {
         this.dataTime = dataTime;
     }
 
-    public String getPm10Value() {
+    public int getPm10Value() {
         return pm10Value;
     }
 
-    public void setPm10Value(String pm10Value) {
+    public void setPm10Value(int pm10Value) {
         this.pm10Value = pm10Value;
     }
 
-    public String getPm10Grade() {
+    public int getPm10Grade() {
         return pm10Grade;
     }
 
-    public void setPm10Grade(String pm10Grade) {
+    public void setPm10Grade(int pm10Grade) {
         this.pm10Grade = pm10Grade;
     }
 
